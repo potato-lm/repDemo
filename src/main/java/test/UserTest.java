313e7c473541defa84e8f3047bd7ba1a6b54e18d
potato_lm <122707877@qq.com>
2019-07-26 08:19:00 +0800

    创建了分支dev

--- src/main/java/test/UserTest.java
+++ src/main/java/test/UserTest.java
@@ -7,5 +7,6 @@ public class UserTest {
         System.out.println("User:"+new User("张三","123456","男"));
         System.out.println("726今早修改了User");
         System.out.println("大家好，我叫王磊，是一名有三年开发经验的程序员");
+        System.out.println("创建了分支dev");
     }
 }
