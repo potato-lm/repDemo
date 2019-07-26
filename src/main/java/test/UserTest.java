0ddd18871033adb814ae185524a53d1318e6040c
potato_lm <122707877@qq.com>
2019-07-26 08:16:02 +0800

    修改了UserTesy

--- src/main/java/test/UserTest.java
+++ src/main/java/test/UserTest.java
@@ -6,5 +6,6 @@ public class UserTest {
     public static void main(String[] args) {
         System.out.println("User:"+new User("张三","123456","男"));
         System.out.println("726今早修改了User");
+        System.out.println("大家好，我叫王磊，是一名有三年开发经验的程序员");
     }
 }
